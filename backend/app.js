99ecb7fcdc9e3389f76f3c9de0ca470165d17fae
IslanP1 <islanpereiradeoliveira@gmail.com>
2024-11-28 23:35:48 -0300

    refactor: ajuste cors

--- backend/app.js
+++ backend/app.js
@@ -5,24 +5,13 @@ import { routes } from "./routes/routes.js";
 import clientdb from "./config/db.js";
 
 const app = express();
-
-// Configuração do CORS
-const corsOptions = {
-    origin: 'http://127.0.0.1:5500',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    allowedHeaders: ['Content-Type', 'Authorization'],
-};
-
-app.use(cors(corsOptions));
 dotenv.config();
-app.use(express.json());
-
 await clientdb.connect();
 
-// Rotas
+app.use(cors());
+app.use(express.json());
 app.use(routes);
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log(`Server is running on port ${process.env.SERVER_PORT}`);
 });
-
